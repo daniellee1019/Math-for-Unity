927a768edf26b72064e108c3839c0288ee51f70c
이정민 <79199117+daniellee1019@users.noreply.github.com>
2021-12-24 16:09:55 +0900

    First2D-Project
    Simple 2D Game

---
+++ FirstWinger/Assets/Scripts/SystemManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    static SystemManager intance = null;
+    // Start is called before the first frame update
+
+    public static SystemManager Instance
+    {
+        get
+        {
+            return intance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(intance != null)
+        {
+            Debug.LogError("SystemManager error! Singletone error!");
+            Destroy(gameObject);
+            return;
+        }
+
+        intance = this;
+    }
+
+    [SerializeField]
+    Player player;
+
+    public Player Hero
+    {
+        get
+        {
+            return player;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
