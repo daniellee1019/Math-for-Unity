caac4f421d0b8d116faabeedf6bf1ffdd311f58f
이정민 <79199117+daniellee1019@users.noreply.github.com>
2022-01-07 21:54:56 +0900

    플레이어 이동 구현 완료
    플레이어 이동 구현 완료

--- 3D Project/Assets/Stylized Astronaut/Character/JoyStick.cs
+++ 3D Project/Assets/Stylized Astronaut/Character/JoyStick.cs
@@ -1,57 +1,95 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.EventSystems; // 키보드, 마우스, 터치를 이벤트로 오브젝트에 보낼 수 있는 기능을 지원
 
-public class JoyStick : MonoBehaviour
+public class JoyStick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Transform Player;
-    public Transform Stick;
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
 
-    private Vector3 StickFirstPos; // 조이스틱 처음 위치
-    private Vector3 JoyVec;        // 조이스틱 벡터(방향)
-    private float Radius;          // 조이스틱 배경의 반지름
-    public bool MoveFlag;
+    [SerializeField, Range(10,150)]
+    private float leverRange; // 레버를 일정 범위 이상으로 못 넘어가게 하기 위한 변수
+
+    private Vector2 inputDirection;
+    private bool isInput;
+    
+    [SerializeField]
+    private Player controller;
+
+
+    public enum JoystickType { Move, Rotate }
+    public JoystickType joystickType;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        Radius = GetComponent<RectTransform>().sizeDelta.y * 0.5f;
-
-        // 캔버스 크기에 대한 반지름 조절
-        float Can = transform.parent.GetComponent<RectTransform>().localScale.x;
-        Radius *= Can;
-
-        MoveFlag = false;
+        
     }
-    private void Update()
+    void Update()
     {
- 
-    }
-    public void Drag(BaseEventData _Data)
-    {
-        PointerEventData Data = _Data as PointerEventData;
-        Vector3 Pos = Data.position;
-
-        // 조이스틱을 이동시킬 방향을 구하기(왼쪽, 오른쪽, 위, 아래)
-        JoyVec = (Pos - StickFirstPos).normalized;
-
-        // 조이스틱의 처음 위치와 현재 내가 터치하고 있는 위치의 거리를 구한다.
-        float Dis = Vector3.Distance(Pos, StickFirstPos);
-
-        // 거리가 반지름보[다 커지면 조이스틱을 반지름의 크기만큼만 이동
-        if (Dis < Radius)
-            Stick.position = StickFirstPos + JoyVec * Dis;
-        else
-            Stick.position = StickFirstPos + JoyVec * Radius;
-
-        Player.eulerAngles = new Vector3(0, Mathf.Atan2(JoyVec.x, JoyVec.y) * Mathf.Rad2Deg, 0);
+        if (isInput)
+        {
+            InputControlVector();
+        }
     }
 
-
-    // 드래그 종료
-    public void DragEnd()
+    public void OnBeginDrag(PointerEventData eventData) // 드래그를 시작할 때
     {
-        Stick.position = StickFirstPos; // 스틱을 원래의 위치로
-        JoyVec += Vector3.zero;         // 방향을 0으로
-        MoveFlag = false;
+        ControlJoystickLever(eventData);
+        isInput = true;
+    }
+
+    public void OnDrag(PointerEventData eventData) // 드래그 중일때 
+    {
+        ControlJoystickLever(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData) // 드래그가 끝날을 때
+    {
+        lever.anchoredPosition = Vector2.zero; // 드래그가 끝나면 처음 위치로 돌아감
+        isInput = false;
+
+        switch (joystickType)
+        {
+            case JoystickType.Move:
+                controller.Move(Vector2.zero);
+                break;
+            // 조이스틱 2개 쓰기위한 switch문
+            //case JoystickType.Rotate:
+                //break;
+        }
+        
+    }
+    private void ControlJoystickLever(PointerEventData eventData) // 레버의 움직임 구현 함수
+    {
+        var inputPos = eventData.position - rectTransform.anchoredPosition; // 터치 위치를 반환해줌
+        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
+        //레버의 위치 길이가 일정 범위보다 작으면 그대로 반환하고, 길면 위치 길이를 정규화 하여 일정 범위를 곱해줘 레버가 일정 범위 밖으로 나가지 못하도록 만들어줌
+        lever.anchoredPosition = inputVector;
+
+        inputDirection = inputVector / leverRange; // inputVector는 해상도를 기반으로 만들어진 값이라 유동적이고 너무 큰 값을 갖고있어 캐릭터의 이동속도로 쓰기엔 너무 빠름
+                                                   // 따라서 값을 0 ~ 1 사이의 값으로 만들어 주어 정규화된 값으로 캐릭터에 전달해야 되기 때문에 나누어줌
+    }
+
+    public void InputControlVector() // 캐릭터에게 입력벡터를 전달하는 함수
+    {
+
+        switch (joystickType)
+        {
+            case JoystickType.Move:
+                controller.Move(inputDirection);
+                break;
+
+            // 조이스틱 2개 쓰기위한 switch문
+            //case JoystickType.Rotate:
+                //controller.LookAround(inputDirection);
+                //break;
+        }
+        
+        //Debug.Log(inputDirection.x + " / " + inputDirection.y);
     }
 }
--- 3D Project/Assets/Stylized Astronaut/Character/Player.cs
+++ 3D Project/Assets/Stylized Astronaut/Character/Player.cs
@@ -2,41 +2,88 @@ using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour {
-		JoyStick joyStick;
-		private Animator anim;
-		private CharacterController controller;
 
-		public float speed = 600.0f;
-		public float turnSpeed = 400.0f;
-		private Vector3 moveDirection = Vector3.zero;
-		public float gravity = 20.0f;
+	[SerializeField]
+	private Transform characterBody;
+	[SerializeField]
+	private Transform cameraArm;
 
 
-		public void Start () 
-			{
-				controller = GetComponent <CharacterController>();
-				anim = gameObject.GetComponentInChildren<Animator>();
-			
-			}
+	private Animator anim;
+	private CharacterController controller;
+    public float gravity = 20.0f;
+
+
+    void Start () 
+	{
+		controller = GetComponent<CharacterController>();
+		anim = characterBody.GetComponent<Animator>();
+	}
 
 	public void Update()
 	{
-		if (joyStick.MoveFlag)
-		{
-			transform.Translate(transform.forward * Time.deltaTime * speed);
-			anim.SetInteger("AnimationPar", 1);
-		}
-		else
-		{
-			anim.SetInteger("AnimationPar", 0);
-		}
 
-		if (controller.isGrounded)
-		{
-			moveDirection = transform.forward * Time.deltaTime * speed;
-		}
-
-
+		
 
 	}
+    public void Move(Vector2 inputDirection)
+    {
+        // 이동 방향 구하기 1
+        //Debug.DrawRay(cameraArm.position, cameraArm.forward, Color.red);
+
+        // 이동 방향 구하기 2
+        //Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
+
+        // 이동 방향키 입력 값 가져오기
+        Vector2 moveInput = inputDirection;
+        // 이동 방향키 입력 판정 : 이동 방향 벡터가 0보다 크면 입력이 발생하고 있는 중
+        bool isMove = moveInput.magnitude != 0;
+        // 입력이 발생하는 중이라면 이동 애니메이션 재생
+        
+        if (isMove)
+        {
+            anim.SetInteger("AnimationPar", 1);
+            // 카메라가 바라보는 방향
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            // 카메라의 오른쪽 방향
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            // 이동 방향
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            // 이동할 때 카메라가 보는 방향 바라보기
+            //characterBody.forward = lookForward;
+            // 이동할 때 이동 방향 바라보기
+            characterBody.forward = moveDir;
+            // 이동
+            controller.Move(moveDir * Time.deltaTime * 6.5f);
+            moveDir.y -= gravity * Time.deltaTime;
+
+        }
+        else
+            anim.SetInteger("AnimationPar", 0);
+    }
+    //  이건 조이스틱 두개 쓸 수도 있으니까 구현해 놓은건데
+    //  오른쪽 조이스틱으로 움직이고 왼쪽 조이스틱으로 화면 움직이는 코드임
+    //public void LookAround(Vector3 inputDirection)
+    //{
+    //    // 마우스 이동 값 검출
+    //    Vector2 mouseDelta = inputDirection;
+    //    // 카메라의 원래 각도를 오일러 각으로 저장
+    //    Vector3 camAngle = cameraArm.rotation.eulerAngles;
+    //    // 카메라의 피치 값 계산
+    //    float x = camAngle.x - mouseDelta.y;
+
+    //    // 카메라 피치 값을 위쪽으로 70도 아래쪽으로 25도 이상 움직이지 못하게 제한
+    //    if (x < 180f)
+    //    {
+    //        x = Mathf.Clamp(x, -1f, 70f);
+    //    }
+    //    else
+    //    {
+    //        x = Mathf.Clamp(x, 335f, 361f);
+    //    }
+
+    //    // 카메라 암 회전 시키기
+    //    cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    //}
 }
